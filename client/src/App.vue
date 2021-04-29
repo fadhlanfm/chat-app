52351a10a469aa388177c0f450da1e666862e290
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-04-29 15:05:39 +0700

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created: function () {
+    this.$http.post('http://localhost:8000/api/test')
+      .then(response => {
+        console.log(response)
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
